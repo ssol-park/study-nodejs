11c926bc73fc3815add84a6eb700667799105f4b
SoraPark <ksttlov@naver.com>
2022-02-18 12:25:34 +0900

    Feat: updated 01_typescript chap 08 ~ 09

--- 01_typescript/08_optionalChaining.ts
+++ 01_typescript/08_optionalChaining.ts
@@ -21,3 +21,6 @@ console.log(userId.id); // abc123
 console.log(userId.info?.email); // undefined
 
 console.log(userIdAndInfo.info?.email); // abbbb
+
+// https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Operators/Optional_chaining
+// 왼쪽 평가대상이 없어도 괜찮은 경우에만 선택적으로 사용
---
+++ 01_typescript/09_copy.ts
@@ -0,0 +1,49 @@
+import * as _ from 'lodash';
+
+interface IOriginal {
+  something: string;
+}
+
+const original: IOriginal = {
+  something: 'good',
+};
+
+// shallow copy
+const shallowCopied = original;
+console.log(shallowCopied === original); // true
+console.log(shallowCopied.something === original.something); // true
+console.log({ shallow: shallowCopied.something, original: original.something });
+
+console.log('--------------------------');
+
+// deep copy - 1. spread operator
+const deepCopied = { ...original };
+
+deepCopied.something = 'bad';
+
+console.log(deepCopied === original); // fasle
+console.log({ deep: deepCopied.something, original: original.something });
+
+console.log('--------------------------');
+
+// deep copy - 2. Object.assign()
+const newDeepCopied = Object.assign({}, original);
+newDeepCopied.something = 'new copy!';
+
+console.log(newDeepCopied === original); // fasle
+console.log({
+  newDeepCopied: newDeepCopied.something,
+  original: original.something,
+});
+
+// --> 1, 2번 방법은 2차원 객체의 deep copy가 이루어 지지 않는다.
+
+console.log('--------------------------');
+
+// deep copy - 3. lodash
+const lodashCopied = _.cloneDeep(original);
+console.log(lodashCopied === original); // false
+
+// References
+// https://velog.io/@recordboy/JavaScript-%EC%96%95%EC%9D%80-%EB%B3%B5%EC%82%ACShallow-Copy%EC%99%80-%EA%B9%8A%EC%9D%80-%EB%B3%B5%EC%82%ACDeep-Copy
+// https://roseline.oopy.io/dev/javascript-back-to-the-basic/shallow-copy-deep-copy
