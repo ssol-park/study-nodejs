e4cf3839e5e10ee481295ae27a0a6d0cb7b61fab
ssol-park <ksttlov.naver@hanmail.net>
2022-02-21 15:38:04 +0900

    Fix: updated 01_typescript, chap 13

--- 01_typescript/13_ervery-some.ts
+++ 01_typescript/13_ervery-some.ts
@@ -1,9 +1,20 @@
-const numbers = [10, 20, 30, 40, 50];
+interface ITodo {
+  id: number;
+  text: string;
+  isDone: boolean;
+}
+
+const todos: ITodo[] = [
+  { id: 1, text: 'JavaScript', isDone: true },
+  { id: 2, text: 'TypeScript', isDone: true },
+  { id: 3, text: 'NodeJS', isDone: false },
+  { id: 4, text: 'Express', isDone: false },
+];
 
 // 모든 요소가 조건에 일치하면 true, 아니면 false
-console.log(numbers.every((n) => n > 0)); // true
-console.log(numbers.every((n) => n > 20)); // fasle
+console.log(todos.every((todo) => todo.isDone)); // false
+console.log(todos.every((todo) => todo.text.length > 5)); // true
 
 // 조건에 일치하는 요소 발견 즉시 true 리턴, 아니면 false
-console.log(numbers.some((n) => n === 10)); // true
-console.log(numbers.some((n) => n < 10)); // false
+console.log(todos.some((todo) => todo.isDone)); // true
+console.log(todos.some((todo) => todo.text.length > 5)); // true
