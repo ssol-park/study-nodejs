5d880fb18afdc32edab8f259acb96748bd7c2e12
ssol-park <ksttlov.naver@hanmail.net>
2022-02-21 15:18:46 +0900

    Feat: updated 01_typescript, chap 04

--- 01_typescript/04_scope.ts
+++ 01_typescript/04_scope.ts
@@ -1,3 +1,5 @@
+// var : 함수레벨스코프
+
 var a = 10;
 
 console.log(a); // 10
@@ -8,6 +10,8 @@ console.log(a); // 10
 
 console.log(a); // 20
 
+// let, const : 블록레벨스코프
+
 let b = 30;
 
 console.log(b); // 30
@@ -17,3 +21,21 @@ console.log(b); // 30
 }
 
 console.log(b); // 30
+
+// 렉시컬스코프
+
+let x = 'global';
+
+const foo = () => {
+  let x = 'local';
+  bar();
+};
+
+const bar = () => {
+  console.log(x);
+};
+
+foo(); // global
+bar(); // global
+
+// https://yeoncoding.tistory.com/39
--- 01_typescript/12_closure.ts
+++ 01_typescript/12_closure.ts
@@ -9,3 +9,6 @@ const outer = () => {
 };
 
 const outerFunc = outer();
+
+// https://developer.mozilla.org/ko/docs/Web/JavaScript/Closures
+// https://velog.io/@proshy/JS%ED%81%B4%EB%A1%9C%EC%A0%B8closure%EC%99%80-%ED%81%B4%EB%A1%9C%EC%A0%B8%EC%9D%98-%EC%82%AC%EC%9A%A9-%EC%98%88%EC%A0%9C
