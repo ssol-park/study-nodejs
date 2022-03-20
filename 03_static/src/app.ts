458b11829de0a959c8c4d7409343929141246a9c
ssol-park <ksttlov.naver@hanmail.net>
2022-03-20 14:48:06 +0900

    Feat: added 03_static

---
+++ 03_static/src/app.ts
@@ -0,0 +1,26 @@
+import express, { NextFunction, Request, Response } from 'express';
+import morgan from 'morgan';
+import path from 'path';
+
+const app = express();
+
+app.set('port', 3000);
+
+app.use(morgan(process.env.NODE_ENV === 'production' ? 'combined' : 'dev'));
+app.use(
+  express.static(path.join(__dirname, 'public'), { extensions: ['html'] })
+);
+
+app.use((req, res) => {
+  res.status(404).send('Not Found');
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err);
+
+  res.status(500).send('Internal Server Error');
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`Server is running at http://localhost:${app.get('port')}`);
+});
